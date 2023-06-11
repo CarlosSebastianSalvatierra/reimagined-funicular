7cc684113304d323b595d3ab489ac72c11eee94d
Franco Gutierrez <francogutierrez04@gmail.com>
2023-06-11 16:56:06 -0300

    Agregue la funcion 'mostrarDetalles' que recupera la informacion del JSON y la agrega al HTML

--- funciones.js
+++ funciones.js
@@ -72,36 +72,119 @@ function redirectToSection(event) {
 }
 
 
-const mostrarDetalles = () => {
-  fetch('datos.json')
+const mostrarDetalles = (posicion) => {
+  fetch("datos.json")
     .then(res => res.json())
     .then(res => {
-      // Obtener la primera capacitación del arreglo
-      const primerCapacitacion = res[0];
-
+      const capacitacion = res[posicion]
+      
       var contenedor = document.getElementById("containerDetalles");
 
-      // Crear elementos HTML para mostrar los detalles
-      var div = document.createElement('div');
-      div.classList.add("container"); // Agregar la clase "container" al div
+      // Vaciar el contenido del contenedor
+      contenedor.innerHTML = "";
 
-      var tituloElement = document.createElement('p');
+      var tituloElement = document.createElement('h2');
       var descripcionElement = document.createElement('p');
+      var presentacionFormulario = document.createElement('h4');
 
       // Asignar los valores del JSON a los elementos HTML
-      tituloElement.textContent = "Título: " + primerCapacitacion.titulo;
-      descripcionElement.textContent = "Descripción: " + primerCapacitacion.descripcion;
+      tituloElement.textContent = capacitacion["titulo"];
+      descripcionElement.textContent = capacitacion["descripcion"];
+      presentacionFormulario.textContent = "Formulario de Preinscripcion"
+
+      tituloElement.classList.add("descripcionCreada");
+      descripcionElement.classList.add("descripcionCreada");
+      presentacionFormulario.classList.add("descripcionCreada");
 
       // Agregar los elementos al div
-      contenedor.appendChild(div);
-      div.appendChild(idElement);
-      div.appendChild(tituloElement);
-      div.appendChild(descripcionElement);
+      contenedor.appendChild(tituloElement);
+      contenedor.appendChild(descripcionElement);
+      contenedor.appendChild(presentacionFormulario);
+
+
+
+
+      //CREACION DEL FORMULARIO
+
+      agregarFormularioBootstrap();
+      
+      
+      contenedor.scrollIntoView({ behavior: 'smooth' });
     })
     .catch(error => {
       console.log('Error:', error);
     });
 }
+
+
+const agregarFormularioBootstrap = () => {
+  var contenedor = document.getElementById("containerDetalles");
+
+  // Crear formulario de Bootstrap
+  var formulario = document.createElement('form');
+  formulario.classList.add('row', 'g-3', 'needs-validation');
+
+  // Campos del formulario
+  var campoNombre = crearCampoFormulario('Nombre', 'nombre', 'text', 'Ingrese su nombre');
+  var campoApellido = crearCampoFormulario('Apellido', 'apellido', 'text', 'Ingrese su apellido');
+  var campoDni = crearCampoFormulario('DNI', 'dni', 'text', 'Ingrese su DNI');
+  var campoEmail = crearCampoFormulario('Email', 'email', 'email', 'Ingrese su correo electrónico');
+  var campoTelefono = crearCampoFormulario('Teléfono', 'telefono', 'text', 'Ingrese su número de teléfono');
+
+
+  // Crear div para el botón Enviar
+  var divBoton = document.createElement('div');
+  divBoton.classList.add('col-md-4', 'offset-md-8', 'text-center');
+
+  // Crear botón Enviar
+  var botonEnviar = document.createElement('button');
+  botonEnviar.type = 'submit';
+  botonEnviar.classList.add('btn', 'btn-primary');
+  botonEnviar.textContent = 'Enviar';
+
+  // Agregar el botón Enviar al div
+  divBoton.appendChild(botonEnviar);
+
+  // Agregar el div al formulario
+  formulario.appendChild(divBoton);
+  
+
+
+  // Agregar campos al formulario
+  formulario.appendChild(campoNombre);
+  formulario.appendChild(campoApellido);
+  formulario.appendChild(campoDni);
+  formulario.appendChild(campoEmail);
+  formulario.appendChild(campoTelefono);
+  // Agregar el botón Enviar al formulario
+  formulario.appendChild(botonEnviar);
+
+  // Agregar el formulario al contenedor
+  contenedor.appendChild(formulario);
+};
+
+// Función auxiliar para crear un campo de formulario
+const crearCampoFormulario = (labelText, name, type, placeholder) => {
+  var campo = document.createElement('div');
+  campo.classList.add('col-md-6');
+
+  var etiqueta = document.createElement('label');
+  etiqueta.htmlFor = name;
+  etiqueta.textContent = labelText;
+
+  var input = document.createElement('input');
+  input.type = type;
+  input.classList.add('form-control');
+  input.name = name;
+  input.placeholder = placeholder;
+  input.required = true;
+
+  campo.appendChild(etiqueta);
+  campo.appendChild(input);
+
+  return campo;
+};
+
 
 
 function agregarLineas() {
