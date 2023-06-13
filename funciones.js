424ca3aa41f13dc263023f7aa0bf4a9f6a5e10d1
Franco Gutierrez <francogutierrez04@gmail.com>
2023-06-13 19:55:40 -0300

    Cambio en la documentacion de los documentos

--- funciones.js
+++ funciones.js
@@ -16,7 +16,7 @@ function zoomOut(img) {
 
 
 function scrollToSectionMargen(event) {
-  event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+  event.preventDefault(); 
   var targetSection = document.querySelector(event.target.getAttribute('href'));
   var offset = 100;
   if (targetSection) {
@@ -70,14 +70,13 @@ const mostrarDetalles = (posicion) => {
 
       agregarFormularioBootstrap();
 
-      const margenSuperior = 80; // Ajusta el valor según tus necesidades
+      const margenSuperior = 80;
 
       contenedor.scrollIntoView({ behavior: 'smooth', block: 'start' });
     
-      // Esperar a que finalice el desplazamiento suave
       setTimeout(() => {
         window.scrollBy(0, -margenSuperior);
-      }, 500); // Ajusta el tiempo de espera según tus necesidades
+      }, 500);
     })
     .catch(error => {
       console.log('Error:', error);
@@ -88,45 +87,35 @@ const mostrarDetalles = (posicion) => {
 const agregarFormularioBootstrap = () => {
   var contenedor = document.getElementById("containerDetalles");
 
-  // Crear formulario de Bootstrap
   var formulario = document.createElement('form');
   formulario.classList.add('row', 'g-3');
 
-  // Campos del formulario
   var campoNombre = crearCampoFormulario('Nombre', 'nombre', 'text', 'Ingrese su nombre');
   var campoApellido = crearCampoFormulario('Apellido', 'apellido', 'text', 'Ingrese su apellido');
   var campoDni = crearCampoFormulario('DNI', 'dni', 'text', 'Ingrese su DNI');
   var campoEmail = crearCampoFormulario('E-Mail', 'email', 'email', 'Ingrese su correo electrónico');
   var campoTelefono = crearCampoFormulario('Teléfono', 'telefono', 'text', 'Ingrese su número de teléfono');
 
-  // Crear div para el botón Enviar
   var divBoton = document.createElement('div');
-  divBoton.classList.add('col-md-12', 'text-center'); // Botón centrado
+  divBoton.classList.add('col-md-12', 'text-center'); 
 
-  // Crear botón Enviar
   var botonEnviar = document.createElement('button');
   botonEnviar.type = 'submit';
-  botonEnviar.classList.add('btn', 'btn-primary', 'btn-block'); // Agregar clase btn-block
+  botonEnviar.classList.add('btn', 'btn-primary', 'btn-block');
   botonEnviar.textContent = 'Preinscribirse';
 
-
-
-  // Agregar campos al formulario
   formulario.appendChild(campoNombre);
   formulario.appendChild(campoApellido);
   formulario.appendChild(campoDni);
   formulario.appendChild(campoEmail);
   formulario.appendChild(campoTelefono);
 
-    // Agregar el botón Enviar al div
-    divBoton.appendChild(botonEnviar);
+  divBoton.appendChild(botonEnviar);
 
-    // Agregar el div al formulario
-    formulario.appendChild(divBoton);
+  formulario.appendChild(divBoton);
 
   formulario.addEventListener('submit', validarFormularioPreiscripcion);
 
-  // Agregar el formulario al contenedor
   contenedor.appendChild(formulario);
 };
 
@@ -155,19 +144,16 @@ const crearCampoFormulario = (labelText, id, type, placeholder) => {
 
 
 function validarFormularioContacto() {
-  // Obtener los valores de los campos
   var nombre = document.getElementById("nombreContacto").value;
   var apellido = document.getElementById("apellidoContacto").value;
   var email = document.getElementById("emailContacto").value;
   var telefono = document.getElementById("telefonoContacto").value;
 
-  // Verificar si los campos están vacíos
   if (nombre === "" || apellido === "" || email === "" || telefono === "") {
     alert("Todos los campos son obligatorios");
     return false;
   }
 
-  // Verificar si el nombre y el apellido tienen más de 2 caracteres
   if (nombre.length < 2 || apellido.length < 2) {
     alert("El nombre y el apellido deben tener al menos 2 caracteres");
     return false;
@@ -178,19 +164,16 @@ function validarFormularioContacto() {
     return false;
   }
 
-  // Verificar si el email contiene un punto y un arroba
   if (email.indexOf(".") === -1 || email.indexOf("@") === -1 || email.length < 6) {
     alert("El email debe contener un punto y un arroba y no puede tener menos de 6 caracteres");
     return false;
   }
 
-  // Verificar si el teléfono tiene 10 dígitos
   if (telefono.length < 10 || isNaN(telefono)) {
     alert("El teléfono debe ser un número de mínimo 10 dígitos");
     return false;
   }
 
-  // Si todas las validaciones son exitosas, se enviará el formulario
   alert("Sus datos se han enviado correctamente");
   return true;
 }
@@ -204,7 +187,6 @@ function validarFormularioPreiscripcion(event) {
   var email = document.getElementById("email").value;
   var telefono = document.getElementById("telefono").value;
 
-  // Validar que el nombre y apellido tengan más de dos caracteres
   if (nombre.length < 2 || apellido.length < 2) {
     alert("El nombre y el apellido deben tener al menos 2 caracteres");
     return false;
@@ -215,32 +197,28 @@ function validarFormularioPreiscripcion(event) {
     return false;
   }
 
-  // Validar que el DNI sea un número
   if (isNaN(dni)) {
     alert("El DNI debe ser un número");
     return false;
   }
 
-  // Validar que el DNI esté entre 1 millón y 99 millones
+
   var dniNumero = parseInt(dni);
   if (isNaN(dniNumero) || dniNumero < 1000000 || dniNumero > 99000000) {
     alert("El DNI debe ser un número y estar entre 1 millón y 99 millones");
     return false;
   }
 
-  // Validar que el email tenga más de 6 caracteres y contenga un arroba y un punto
   if (email.length < 6 || email.indexOf("@") === -1 || email.indexOf(".") === -1) {
     alert("El email debe tener más de 6 caracteres y contener un arroba (@) y un punto (.)");
     return false;
   }
 
-  // Validar que el teléfono tenga al menos 10 cifras
   if (telefono.length < 10 || isNaN(telefono)) {
     alert("El teléfono debe ser un número de al menos 10 cifras");
     return false;
   }
 
-  // Si todas las validaciones pasan, se puede enviar el formulario
   alert("Se han registrado sus datos")
   return true;
 }
